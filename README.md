98d1822170d83e64a9cdb82d7071d3b8fb999a3c
bj bautista <emailme@djbrigidope.com>
2017-07-25 14:35:23 -0700

    putting away my toys.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # threejayess
 i should be testing up but working on this instead
+
+
+
+2:31pm an interesting well to fall into. shall come back after testing. 
--- stuff.js
+++ stuff.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -21,4 +21,4 @@ var animate = function () {
 	renderer.render(scene, camera);
 };
 
-animate();
+animate(0);
